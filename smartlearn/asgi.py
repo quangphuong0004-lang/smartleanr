cc5367e1258c34d059795618d2a4bd112545a02f
Dam Phuong <quangphuong0004@gmail.com>
2026-03-09 16:01:27 +0700

    feat: update set up

--- smartlearn/asgi.py
+++ smartlearn/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for smartlearn project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartlearn.settings')
 
-application = get_asgi_application()
+# Import routing sau khi set env
+from chat.routing import websocket_urlpatterns as chat_ws
+from live_quiz.routing import websocket_urlpatterns as live_ws
+from notifications.routing import websocket_urlpatterns as notif_ws
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat_ws + live_ws + notif_ws
+        )
+    ),
+})
